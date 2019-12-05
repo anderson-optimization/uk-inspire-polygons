0b14a9974f93e675b109cfa5f44a2c54420646e9
Eric Anderson <ericjanderson4@gmail.com>
2019-12-04 21:46:13 -0700

    feat: Create merged shapefile for upload to postgres

--- src/merge.sh
+++ src/merge.sh
@@ -1,13 +1,13 @@
-for file in */*.gml
 OGR="docker run -v $(pwd):/tmp/work2 -w /tmp/work2 andersonopt/geoanalysis ogr2ogr"
+for file in */*.gml
 do
  echo "File ${file}"
  if [ -d merged ]
  then
   echo "Appending merged shape"
-  OGR -update -append -f "ESRI Shapefile" -t_srs EPSG:4326 merged Abertawe_-_Swansea/Land_Registry_Cadastral_Parcels.gml -s_srs EPSG:27700
+  ${OGR} -update -append -f "ESRI Shapefile" -t_srs EPSG:4326 merged Abertawe_-_Swansea/Land_Registry_Cadastral_Parcels.gml -s_srs EPSG:27700
  else
   echo "Creating merged shape"
-  OGR -f "ESRI Shapefile" -t_srs EPSG:4326 merged Abertawe_-_Swansea/Land_Registry_Cadastral_Parcels.gml -s_srs EPSG:27700
+  ${OGR} -f "ESRI Shapefile" -t_srs EPSG:4326 merged Abertawe_-_Swansea/Land_Registry_Cadastral_Parcels.gml -s_srs EPSG:27700
  fi
 done
